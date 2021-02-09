9b1a297f8494743ba486128501628d8361e1b58b
CristianCin <64826273+CristianCin@users.noreply.github.com>
2021-02-09 18:21:15 +0100

    Add files via upload

--- evaluate.hpp
+++ evaluate.hpp
@@ -1,70 +1,12 @@
-#include <bits/stdc++.h>
-using namespace std;
- 
+/* C++ implementation to convert 
+infix expression to postfix*/
+// Note that here we use std::stack 
+// for Stack operations 
+#include<bits/stdc++.h> 
+using namespace std; 
+
 #define PI 3.14159265
-
-int precedence(string op){
-    if(op == "+" || op == "-")
-        return 1;
-    if(op == "*" || op == "/")
-        return 2;
-    if(op == "^") 
-        return 3;
-
-    return 0;
-}
- 
-double applyOp(double a, double b, string op){
-
-    if(op == "+") return a + b;
-    if(op == "-") return a - b;
-    if(op == "*") return a * b;
-    if(op == "/") return a / b;
-    if(op == "^") return pow(a,b);
-
-    return 0;
-}
-
-float cbl(float base, float arg){
-    return (float)log10(arg)/(float)log10(base);
-}
-
-double applyOpF(double a, string op){
-
-    if(op == "sin") return sin(a*(PI/180));
-    if(op == "cos") return cos(a*(PI/180));
-    if(op == "tan") return tan(a*(PI/180));
-    if(op == "cotan") return 1/tan(a*(PI/180));
-    if(op == "arcsin") return asin(a);
-    if(op == "arccos") return acos(a);
-    if(op == "arctan") return atan(a);
-    if(op == "arccotan") return PI/2 - atan(a);
-    if(op == "sqrt") return sqrt(a);
-    if(op == "log_2") return log2(a);
-    if(op == "ln") return log(a);
-    if(op == "log_3") return cbl(3,a);
-    if(op == "log_4") return cbl(4,a);
-    if(op == "log_5") return cbl(5,a);
-    if(op == "log_6") return cbl(6,a);
-    if(op == "log_7") return cbl(7,a);
-    if(op == "log_8") return cbl(8,a);
-    if(op == "log_9") return cbl(9,a);
-    if(op == "log") return log10(a);
-
-    return 0;
-}
-
-bool isOperand(char ch){
-    switch(ch){
-        case '+': return true;
-        case '-': return true;
-        case '*': return true;
-        case '/': return true;
-        case '^': return true;
-    }
-
-    return false;
-}
+bool dbg = false;
 
 bool isF(string op){
 
@@ -91,21 +33,105 @@ bool isF(string op){
     return false;
 }
 
+//Function to return precedence of operators 
+int prec(string c) 
+{ 
+    if(isF(c))
+        return 4;
+	if(c == "^") 
+	    return 3; 
+	
+    if(c == "*" || c == "/") 
+	    return 2; 
+	
+    if(c == "+" || c == "-") 
+	    return 1; 
+
+	return -1; 
+} 
+
+bool isO(string op){
+    return (prec(op) > 0 || isF(op))? true : false;
+}
+
+// The main function to convert infix expression 
+//to postfix expression 
+vector<string> infixToPostfix(vector<string> s) 
+{ 
+	stack<string> st;  
+	int l = s.size(); 
+	vector<string> ns; 
+	for(int i = 0; i < l; i++) 
+	{
+		if(isO(s[i]) == false && isF(s[i]) == false && s[i] != "(" && s[i] != ")") 
+		    ns.push_back(s[i]); 
+
+		else 
+            if(s[i] == "(") st.push("("); 
+
+		else if(s[i] == ")") 
+		{ 
+			while(!st.empty() && st.top() != "(") 
+			{  
+			    ns.push_back(st.top()); 
+				st.pop(); 
+			} 
+			if(st.top() == "(") 
+				st.pop(); 
+		} 
+		else{ 
+			while(!st.empty() && prec(s[i]) < prec(st.top())) 
+			{ 
+			    ns.push_back(st.top()); 
+				st.pop(); 
+			} 
+
+			st.push(s[i]); 
+		} 
+
+	} 
+	while(!st.empty()) 
+	{ 
+        ns.push_back(st.top()); 
+    	st.pop(); 
+	} 
+	
+	return ns;
+
+} 
+
+bool is_number(string number_string)
+{
+    string::size_type sz;
+    bool is_valid;
+    double parsed_number = 0.0;
+    try 
+    { 
+        parsed_number = std::stod(number_string, &sz); 
+        is_valid = true; 
+    } 
+    catch(std::exception& ia) 
+	{ 
+        is_valid = false; 
+    } 
+
+    return is_valid;
+}
+
 vector<string> tokenization(string token){
     string tmp = "";
     vector<string> ret;
-    
-    for(auto x : token){
-        if(x != ' ')  
-            if(isOperand(x) || x == '(' || x == ')' ){
+    for(int i = 0; i < token.size(); i++){
+        string x = string(1, token[i]) ;
+        if(x != " "){
+            if(isO(x) || x == "(" || x == ")" ){
                     ret.push_back(tmp);
-                    tmp = x;
-                    ret.push_back(tmp);
+                    ret.push_back(x);
                     tmp = "";
             }
             else
                 tmp += x;
-        else
+        }else
             continue;
     }
 
@@ -117,95 +143,91 @@ vector<string> tokenization(string token){
         if(x != "")
             retret.push_back(x);
 
-    return retret;
+    vector<string> newretret;
+    bool flag = false;
+    for(int i = 0; i < retret.size(); i++)
+        if(i > 0 && i + 1 < retret.size() && (isO(retret[i-1]) || retret[i-1] == "(") && retret[i] == "-" && is_number(retret[i+1]))
+            newretret.push_back("-" + retret[i+1]), i++;
+        else        
+            newretret.push_back(retret[i]);
+
+    return newretret;
 }
 
-bool is_number(string s)
-{
-    return !s.empty() && std::find_if(s.begin(), s.end(), [](unsigned char c) { return !std::isdigit(c); }) == s.end();
-}
- 
-void update(stack<double> &values, stack<string> &_operators){
-    double value1, value2;
-    string _operator;
+double apply(double a, double b, string op){
 
-    value2 = values.top(); //Prendo il valore in cima allo stack (Primo numero)
-    values.pop(); // Lo rimuovo
-    
-    value1 = values.top(); //Prendo il valore in cima allo stack (Secondo numero)
-    values.pop(); //Lo rimuovo
-    
-    _operator = _operators.top(); //Prendo l'operatore in cima allo stack
-    _operators.pop(); //Lo rimuovo
-    
-    values.push(applyOp(value1, value2, _operator));
+    if(op == "+") return a + b;
+    if(op == "-") return a - b;
+    if(op == "*") return a * b;
+    if(op == "/") return a / b;
+    if(op == "^") return pow(a,b);
+
+    return 0;
 }
 
-void updateFunction(stack<double> &values, stack<string> &_operators){
-    double value;
-    string _operator;
-
-    value = values.top(); //Prendo il valore in cima allo stack (Numero interno alla funzione)
-    values.pop(); // Lo rimuovo
-    
-    _operator = _operators.top(); //Prendo l'operatore in cima allo stack
-    _operators.pop(); //Lo rimuovo
-    
-    values.push(applyOpF(value, _operator));
+double cbl(double base, double arg){
+    return (double)log10(arg)/(double)log10(base);
 }
 
-double evaluate(string tokens){
-    vector<string> tk = tokenization(tokens);
-    double value;
-    char _operator;
-    stack <double> values;
-     
-    stack <string> ops;
-    char x;
-    for(int i = 0; i < tk.size(); i++){
+double applyF(double a, string op){
 
-        if(tk[i] == "(")
-            ops.push(tk[i]);
-        else 
-            if(is_number(tk[i])){
-                int val = stoi(tk[i]);
-                values.push(val);
+    if(op == "sin") return sin((a/180)*PI);
+    if(op == "cos") return cos((a/180)*PI);
+    if(op == "tan") return tan((a/180)*PI);
+    if(op == "cotan") return 1/tan((a/180)*PI);
+    if(op == "arcsin") return asin(a) * 180.0 / PI;
+    if(op == "arccos") return acos(a) * 180.0 / PI;
+    if(op == "arctan") return atan(a) * 180.0 / PI;
+    if(op == "arccotan") return PI/2 - atan(a);
+    if(op == "sqrt") return sqrt(a);
+    if(op == "log_2") return log2(a);
+    if(op == "ln") return log(a);
+    if(op == "log_3") return cbl(3,a);
+    if(op == "log_4") return cbl(4,a);
+    if(op == "log_5") return cbl(5,a);
+    if(op == "log_6") return cbl(6,a);
+    if(op == "log_7") return cbl(7,a);
+    if(op == "log_8") return cbl(8,a);
+    if(op == "log_9") return cbl(9,a);
+    if(op == "log") return log10(a);
+
+    return 0;
+}
+
+
+double evaluate(string expr){
+    vector<string> tokens = tokenization(expr);
+    vector<string> post = infixToPostfix(tokens);
+    if(dbg){
+        for(int i = 0; i < post.size(); i++)
+            cout<<"["<<post[i]<<"]";
+        cout<<endl;
+    }
+
+    stack<double> values;
+    for(int i = 0; i < post.size(); i++){
+        if(isF(post[i]) || isO(post[i])){
+            if(isF(post[i])){
+                double value = values.top();
+                values.pop();
+                values.push(applyF(value, post[i]));
+            }else{
+                double value1 = values.top();
+                values.pop();
+                double value2 = values.top();
+                values.pop();
+                values.push(apply(value2, value1, post[i]));
             }
-            else 
-                if(tk[i] == ")")
-                {
-                    while(!ops.empty() && ops.top() != "("){
-                        if(isF(ops.top()))
-                            updateFunction(values, ops);
-                        else
-                            if(precedence(ops.top())>= precedence(tk[i]))
-                                update(values, ops);
-                    }
+        }else
+            values.push(stold(post[i]));
+        
+        if(dbg){
+            for(stack<double> tmp = values; !tmp.empty(); tmp.pop())
+                cout<<tmp.top()<<" ";
 
-                    if(!ops.empty())
-                        ops.pop();
-                }
-                else
-                {
-                    while(!ops.empty() && ops.top() != "("){
-                        if(isF(ops.top()))
-                            updateFunction(values, ops);
-                        else{
-                            if(precedence(ops.top())>= precedence(tk[i]))
-                                update(values, ops);
-                        }
-
-                    }
-                
-                    ops.push(tk[i]);
-                }
+            cout<<endl;
         }
+    }
 
-        while(!ops.empty())
-            if(isF(ops.top()))
-                updateFunction(values, ops);
-            else
-                update(values, ops);
-     
     return values.top();
 }
--- main.cpp
+++ main.cpp
@@ -2,40 +2,8 @@
 * @Author CristianCin
 */
 
-#include <iostream>
-#include <cstdlib>
-#include <vector>
-#include <algorithm>
-#include <ctime>
-#include "evaluate.hpp"
+#include "function.hpp"
 using namespace std;
-
-int levelOfDifficult, numberOfInnerCompositions = 2;
-string f;
-bool debug = false;
-bool flag = false;
-char ope;
-vector<string> functions = {
-    "sin", "cos", "tan", "cotan",
-    "arcsin", "arccos", "arctan", "arccotan",
-    "sqrt", "x", "e", "number", "pi",
-    "log", "ln", "log_",
-    "x", "x", "x", "x",
-    "x", "x", "x", "x",
-    };
-
-vector<char> op = {'+', '-', '*', '/', '^'};
-
-string generatefunction(int);
-string generateFunction();
-string generatenumber();
-string generatefraction();
-string generateExp();
-string generateDecimal();
-string generateInt(string);
-string deleteLastZeros(string);
-char generateOperator();
-int find(string, char);
 
 int main(){
 
@@ -50,175 +18,29 @@ int main(){
         <<"\nvery hard \t>=5\n";
     cin>>levelOfDifficult;
     }else
-        levelOfDifficult = 2;
+        levelOfDifficult = 3;
     srand(time(NULL));
     fun = generatefunction(levelOfDifficult);
     cout<<"f(x) = " <<fun<<endl;
+    cout<<"Graph: (y/n) ";
+    char choice;
+    cin>>choice;
+    if(choice == 'y'){
+        vector<double> x, y;
+        for(double i = 0; i <= 15; i = i + 0.5){
+            string newstr = change(fun, i);
+            double num;
+            num = evaluate(newstr);
+            if(!isnan(num))
+                x.push_back(i),y.push_back(num); 
+            newstr = change(fun, -i);
+            num = evaluate(newstr);
+            if(!isnan(num))
+                x.push_back(-i),y.push_back(num); 
+        }
+
+        for(int i = 0; i < x.size(); i= i + 2)
+            cout<<"("<<x[i]<<", "<<y[i]<<") ("<<x[i+1]<<", "<<y[i+1]<<")\n";
+    }
     return 0;
-}
-
-string generatefunction(int numberOfCompositions){
-    
-    bool condPI = false;
-    int openBrackets = 0;
-    int closeBrackets = 0;
-    string function = "";
-    for(int i = 0; i < numberOfCompositions; i++){
-        ope = generateOperator();
-        int random = rand()%numberOfInnerCompositions;
-        f = generateFunction();
-        flag = false;
-            if(f == "pi"){
-                function += "\u03C0"; 
-                condPI = true;
-            }
-            else
-                if(f == "x" || f == "e")
-                    function += f;
-                else        
-                        if(f == "number")
-                            function += ((rand()%100)%2 == 0)? generatefraction() : generatenumber();
-                        else{
-                            if(f != "log_")
-                                function += f;
-                            else
-                                function += f + to_string(rand()%8 + 2);
-
-                            if(ope != '^')
-                                function += "(", openBrackets++;
-                            else{
-                                flag = true;
-                                function += "((";
-                                openBrackets += 2;
-                            }
-                            if(f == "arccos" || f == "arcsin" || f == "arctan" || f == "arccotan")
-                                function += generateDecimal();
-                            else{
-                                if(f == "cotan" || f == "tan")
-                                    function += generateInt(f);
-                                else
-                                    function += (!random)? generateExp() : generatefunction(random);
-                            }
-    
-                            function += ")";
-                            closeBrackets++;
-                        }
-                    
-            if(ope == '^'){
-                function += "^(";
-                openBrackets++;
-                function += (!random)? generateExp() : generatefunction(random);
-                function += ")";
-                closeBrackets++;
-                if(flag)
-                    function += ")", closeBrackets++;
-
-                while(ope != '^')
-                    ope = generateOperator();
-
-                if(i + 1 < numberOfCompositions)
-                    function += ope;
-            }else
-                if(i + 1 < numberOfCompositions)
-                    function += ope;
-    }
-
-    while(openBrackets > closeBrackets){
-        function += ")";
-        closeBrackets++;
-    }
-
-    if(condPI == false && count(function.begin(), function.end(), 'x') == 0 && count(function.begin(), function.end(), 'e') == 0)
-        return to_string(evaluate(function)); 
-
-    return function;
-}
-
-char generateOperator(){
-    return op[rand()%(op.size())];
-}
-
-string generatenumber(){
-    return to_string(rand()%100 + 1);
-}
-
-int MCD(int a, int b)
-{
-    int r;
-    r = a % b;
-    while (r>0){
-        a = b;
-        b = r;
-        r = a % b;
-    }
-    return b;
-}
-
-string generatefraction(){
-    int num = rand()%10;
-    int den = rand()%10 + 1;
-
-    int mcd = MCD(num, den);
-    num /= mcd;
-    den /= mcd;
-
-    if(num == 0)
-        return "0";
-    if(num == den)
-        return "1";
-    if(den == 1)
-        return to_string(num);
-                
-    return "("+to_string(num)+"/"+to_string(den)+")";
-    
-}
-
-string generateFunction(){
-    return functions[rand()%(functions.size())];
-}
-
-string generateExp(){
-    string res = functions[rand()%4 + 9];
-    res = (res == "number")? ((rand()%100)%2 == 0)? generatefraction() : generatenumber() : res; 
-    res = (res == "pi")? "\u03C0" : res;
-    return res;
-}
-
-int find(string a, char ch){
-    for(int i = 0; i < a.size(); i++)
-        if(a[i] == ch)
-            return i;
-
-    return -1;
-}
-
-string generateDecimal(){
-    double n = rand()%32000 + 1;
-    double divisor = pow(10, 5);
-    double nn = n/divisor;
-    return deleteLastZeros(to_string(nn));
-}
-
-string deleteLastZeros(string str){
-    string newStr = "";
-
-    for(int i = str.size() - 1; i > -1; i--)
-        if(str[i] == '0')
-            newStr = str + newStr;
-        else
-            break;
-
-    return newStr;
-}
-
-string generateInt(string f){
-    double n;
-    if(f == "tan")
-        n = rand()%89999;
-    if(f == "cotan")
-        n = rand()%90000 + 1;
-
-    double nn = n/(pow(10, 5));
-    return deleteLastZeros(to_string(nn));
-    
 }
